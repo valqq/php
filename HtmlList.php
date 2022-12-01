04d7ce14340ec25921b49c8a8b7e21f7917d9e0f
valqq <valentin1.romanov@yandex.ru>
2022-12-01 19:41:21 +0700

    70.1

---
+++ HtmlList.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'Tag.php';
+require_once 'ListItem.php';
+	class HtmlList extends Tag
+	{
+		private $items = [];
+
+		public function addItem(ListItem $li)
+		{
+			$this->items[] = $li;
+			return $this;
+		}
+
+		public function show()
+		{
+			$result = $this->open();
+
+			foreach ($this->items as $item) {
+				$result .= $item->show();
+			}
+
+			$result .= $this->close();
+
+			return $result;
+		}
+		public function __toString()
+		{
+			return $this->show();
+		} 
+	}
+?>
