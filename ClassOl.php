6998ff0f1265e84c2aadc6caa8fd2415eb09d807
valqq <valentin1.romanov@yandex.ru>
2022-12-01 19:40:45 +0700

    70.2-3

---
+++ ClassOl.php
@@ -0,0 +1,8 @@
+<?php
+class Ol extends HtmlList {
+	public function __construct()
+	{
+		parent::__construct('ol');
+	}
+}
+?>
---
+++ ClassUL.php
@@ -0,0 +1,8 @@
+<?php
+class UL extends HtmlList {
+	public function __construct()
+	{
+		parent::__construct('ul');
+	}
+}
+?>
