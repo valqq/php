124a2823c65cc5566f82057e7f817121ff00af92
valqq <valentin1.romanov@yandex.ru>
2022-12-01 19:41:43 +0700

    72.1-4

---
+++ Input.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'Tag.php';
+	class Input extends Tag
+	{
+		public function __construct()
+		{
+			parent::__construct('input');
+		}
+
+		public function open()
+		{
+			$inputName = $this->getAttr('name');
+			if ($inputName) {
+				if (isset($_REQUEST[$inputName])) {
+					$value = $_REQUEST[$inputName];
+					$this->setAttr('value', $value);
+				}
+			}
+
+			return parent::open(); 
+		}
+
+		public function __toString()
+		{
+			return $this->open(); 
+		}
+	}
+?>
