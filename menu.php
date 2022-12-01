6cad2c57b2078747116d1e80e4735b6b86c5fa68
valqq <valentin1.romanov@yandex.ru>
2022-12-01 19:49:22 +0700

    69.1-5

---
+++ menu.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'Link.php';
+	$link1 = (new Link())->setAttr('href', '/php/56-78/1.php')->setText('страница 1');
+	$link2 = (new Link())->setAttr('href', '/php/56-78/2.php')->setText('страница 2');
+	$link3 = (new Link())->setAttr('href', '/php/56-78/3.php')->setText('страница 3');
+	$link4 = (new Link())->setAttr('href', '/php/56-78/4.php')->setText('страница 4');
+	$link5 = (new Link())->setAttr('href', '/php/56-78/5.php')->setText('страница 5');
+?> 
