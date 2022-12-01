8d19b036ae23c17dd34ef75526c6786aacc6d91d
valqq <valentin1.romanov@yandex.ru>
2022-12-01 19:41:34 +0700

    68.1-3

---
+++ Image.php
@@ -0,0 +1,15 @@
+<?php
+	require_once 'tag.php';
+	class Image extends Tag 
+	{
+		public function __construct()
+		{
+			$this->setAttr('src', '')->setAttr('alt', '');
+			parent::__construct('img');
+		}
+		public function __toString()
+		{
+			return parent::open(); 
+		}
+	}
+?>
