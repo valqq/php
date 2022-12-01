00fe7a7c163ac95bb9badf51added2ff35864b48
valqq <valentin1.romanov@yandex.ru>
2022-12-01 19:47:24 +0700

    70.1

---
+++ ListItem.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'Tag.php';
+	class ListItem extends Tag
+	{
+		public function __construct()
+		{
+			parent::__construct('li');
+		} 
+		public function __toString() 
+		{
+			return parent::show();
+		} 
+	}
+?>
